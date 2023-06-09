8169902c74b741c1bbe7538a2a7e4f276c7bf622
bc990424 <65002008+bc990424@users.noreply.github.com>
2023-06-09 10:52:20 +0900

    Add files via upload

---
+++ 20816 안관준.py
@@ -0,0 +1,50 @@
+'''
+print("20816 안관준")
+
+m = int(input('국어'))
+e = int(input('영어'))
+if k+m+d <= 7:
+    print('지원가능')
+else:
+    print('지원불가')
+
+age=int(input('나이:'))
+tall=int(input('키:'))
+
+if age <10 and tall>= 120:
+    print("탑승가능")
+
+else:
+    print("탑승불가")
+
+
+
+sum = 0
+n = int(input('숫자:'))
+for i in range(1, n+1):
+    print(i)
+'''
+
+a = int(input('숫자1:'))
+b = int(input('숫자2:'))
+c = input('+-*/')
+
+if c == '+':
+    print(a+b)
+elif c =='-':
+    print(a-b)
+elif c =='*':
+    print(a*b)
+else:
+    print(a/b)
+
+
+지 = int(input('지불한금액:'))
+물 = int(input('물건값:'))
+거=지-물
+print(거)
+오백원=거/500
+print(오백원)
+6
+
+    
